146ec88c40efa3d1328c0287dd1328b26d92494e
Hiroaki Terayama <hiro.nnsa724@gmail.com>
2019-09-11 16:29:44 +0900

    rspecをリファクタリング

--- nabea2.rb
+++ nabea2.rb
@@ -6,137 +6,141 @@ class Nabea2
   end
 end
 
-# コメントアウトを外して下記テストを全てパスしてください
+# # コメントアウトを外して下記テストを全てパスしてください
 # describe Nabea2 do
 #   context '3の倍数の時おかしくなる' do
 #     let(:crazy_num) { 3 }
+#     let(:nabea2) { Nabea2.new(crazy_num) }
 #     it '0' do
 #       num = 0
-#       expect(Nabea2.new(crazy_num).crazy?(num)).to eq false
+#       expect(nabea2.crazy?(num)).to eq false
 #     end
 
 #     it '3' do
 #       num = 3
-#       expect(Nabea2.new(crazy_num).crazy?(num)).to eq true
+#       expect(nabea2.crazy?(num)).to eq true
 #     end
 
 #     it '6' do
 #       num = 6
-#       expect(Nabea2.new(crazy_num).crazy?(num)).to eq true
+#       expect(nabea2.crazy?(num)).to eq true
 #     end
 
 #     it '13' do
 #       num = 13
-#       expect(Nabea2.new(crazy_num).crazy?(num)).to eq true
+#       expect(nabea2.crazy?(num)).to eq true
 #     end
 
 #     it '60' do
 #       num = 60
-#       expect(Nabea2.new(crazy_num).crazy?(num)).to eq true
+#       expect(nabea2.crazy?(num)).to eq true
 #     end
 
 #     it '100' do
 #       num = 100
-#       expect(Nabea2.new(crazy_num).crazy?(num)).to eq false
+#       expect(nabea2.crazy?(num)).to eq false
 #     end
 #   end
 
 #   context '6の倍数の時おかしくなる' do
 #     let(:crazy_num) { 6 }
+#     let(:nabea2) { Nabea2.new(crazy_num) }
 #     it '0' do
 #       num = 0
-#       expect(Nabea2.new(crazy_num).crazy?(num)).to eq false
+#       expect(nabea2.crazy?(num)).to eq false
 #     end
 
 #     it '3' do
 #       num = 3
-#       expect(Nabea2.new(crazy_num).crazy?(num)).to eq false
+#       expect(nabea2.crazy?(num)).to eq false
 #     end
 
 #     it '6' do
 #       num = 6
-#       expect(Nabea2.new(crazy_num).crazy?(num)).to eq true
+#       expect(nabea2.crazy?(num)).to eq true
 #     end
 
 #     it '13' do
 #       num = 13
-#       expect(Nabea2.new(crazy_num).crazy?(num)).to eq false
+#       expect(nabea2.crazy?(num)).to eq false
 #     end
 
 #     it '60' do
 #       num = 60
-#       expect(Nabea2.new(crazy_num).crazy?(num)).to eq true
+#       expect(nabea2.crazy?(num)).to eq true
 #     end
 
 #     it '100' do
 #       num = 100
-#       expect(Nabea2.new(crazy_num).crazy?(num)).to eq false
+#       expect(nabea2.crazy?(num)).to eq false
 #     end
 #   end
 
 #   context '100の倍数の時おかしくなる' do
 #     let(:crazy_num) { 100 }
+#     let(:nabea2) { Nabea2.new(crazy_num) }
 #     it '0' do
 #       num = 0
-#       expect(Nabea2.new(crazy_num).crazy?(num)).to eq false
+#       expect(nabea2.crazy?(num)).to eq false
 #     end
 
 #     it '3' do
 #       num = 3
-#       expect(Nabea2.new(crazy_num).crazy?(num)).to eq false
+#       expect(nabea2.crazy?(num)).to eq false
 #     end
 
 #     it '6' do
 #       num = 6
-#       expect(Nabea2.new(crazy_num).crazy?(num)).to eq false
+#       expect(nabea2.crazy?(num)).to eq false
 #     end
 
 #     it '13' do
 #       num = 13
-#       expect(Nabea2.new(crazy_num).crazy?(num)).to eq false
+#       expect(nabea2.crazy?(num)).to eq false
 #     end
 
 #     it '60' do
 #       num = 60
-#       expect(Nabea2.new(crazy_num).crazy?(num)).to eq false
+#       expect(nabea2.crazy?(num)).to eq false
 #     end
 
 #     it '100' do
 #       num = 100
-#       expect(Nabea2.new(crazy_num).crazy?(num)).to eq true
+#       expect(nabea2.crazy?(num)).to eq true
 #     end
 #   end
 
 #   context '0の倍数の時おかしくなる' do
 #     let(:crazy_num) { 0 }
+#     let(:nabea2) { Nabea2.new(crazy_num) }
 #     it '0' do
 #       num = 0
-#       expect(Nabea2.new(crazy_num).crazy?(num)).to eq true
+#       expect(nabea2.crazy?(num)).to eq true
 #     end
 
 #     it '3' do
 #       num = 3
-#       expect(Nabea2.new(crazy_num).crazy?(num)).to eq true
+#       expect(nabea2.crazy?(num)).to eq true
 #     end
 
 #     it '6' do
 #       num = 6
-#       expect(Nabea2.new(crazy_num).crazy?(num)).to eq true
+#       expect(nabea2.crazy?(num)).to eq true
 #     end
 
 #     it '13' do
 #       num = 13
-#       expect(Nabea2.new(crazy_num).crazy?(num)).to eq true
+#       expect(nabea2.crazy?(num)).to eq true
 #     end
 
 #     it '60' do
 #       num = 60
-#       expect(Nabea2.new(crazy_num).crazy?(num)).to eq true
+#       expect(nabea2.crazy?(num)).to eq true
 #     end
 
 #     it '100' do
 #       num = 100
-#       expect(Nabea2.new(crazy_num).crazy?(num)).to eq true
+#       expect(nabea2.crazy?(num)).to eq true
 #     end
 #   end
 # end
